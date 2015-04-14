e66bd042c02eaa1dc1c60fc501faf49b959c9d3b
gomito158 <dssb_1989@hotmail.com>
2015-04-14 16:58:11 -0500

    commit con carga de imagenes par cada home

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -27,5 +27,8 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+		
     ];
+	
+	
 }
--- views/hombres-deportes/dephrugby.php
+++ views/hombres-deportes/dephrugby.php
@@ -1,8 +1,111 @@
 <?php
 
+$this->title = 'Rugby';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<link href="css/carousel.css" rel="stylesheet">
 <div class="site-about" style="float: left; margin: 0px 0px 0px 8%">
 	<p>
         esto es deporte hombre rugby => Sitio en Construcci&oacute;n
     </p>
 </div>
+<!-- Carousel
+carrusel de imagenes de portada
+    ================================================== -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel" style="float: left; margin: 0px 0px 0px 6.7%"> 
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="item active"> <img class="first-slide"
+					src="images/150306-start-zpump-sv-2.jpg" alt="First slide"> </div>
+    <div class="item"> <img class="second-slide"
+					src="images/150309-start-Essentials-sv.jpg"
+					alt="Second slide"> </div>
+    <div class="item"> <img class="third-slide"
+					src="images/adi_Stadium_se_Stellasport_banner_1024x600_CTA.jpg"
+					alt="Third slide"> </div>
+    <div class="item"> <img class="third-slide" src="images/gimlet-sv-NY.jpg"
+					alt="Third slide"> </div>
+    <div class="item"> <img class="third-slide"
+					src="images/nike-for-her-JDI-weekend-1024x600.jpg"
+					alt="Third slide"> </div>
+    <div class="item"> <img class="third-slide"
+					src="images/PA6710_40402_Stadium-Online_Bolt_Running_1024x600px.jpg"
+					alt="Third slide"> </div>
+    <div class="item"> <img class="third-slide"
+					src="images/stadium-1024x600-2.png" alt="Third slide"> </div>
+    <div class="item"> <img class="third-slide"
+					src="images/ultra-boost-stadium-1024x600.png"
+					alt="Third slide"> </div>
+  </div>
+   
+</div>
+<!-- /.carousel --> 
+
+<div style="float: left; margin: 0px 0px 0px 8%">
+  <div 
+	style="float: left; margin: 0px 0px 0px 8%"		>
+    <dl class="tips" cssname=""
+				style="float: left; margin: 0px 0px 0px -18%"	>
+      <table cellpadding="0" cellspacing="0" style="width: 20px;">
+        <tbody>
+          <tr>
+            <td><img style="margin: 0% 0% 0% 8%;"
+								src="images/150310-produktlist.jpg"
+								alt="150219-productlist-sneakermania-1"></td>
+          </tr>
+          <tr>
+            <td><div style="float: left; margin: 0px 0px 0px 8%;"> <span style="text-transform: uppercase; font-weight: bold;"></span> <br>
+                <br>
+                <table width="930px">
+                  <tbody>
+                    <tr>
+                      <td style="text-align: center;"><a
+													class="prod-thumb-link"
+													href="http://www.stadium.se/skor/damskor/sneakers/142193/converse-all-star-hi?SelectedProductUUID=n_cKd3emgfkAAAE782ky8qt7#.VPa9CbPF-dw"> <img id="prod_thumb" title="CONVERSE" class="prod_thumb"
+														src="images/142193.jpg" alt=""> <br>
+                        <strong>CONVERSE</strong>
+                        <div style="text-align: center;"> <span class="name">ALL STAR HI</span> </div>
+                        <span class="price"></span> </a>
+                        <div style="text-align: center;"> <a class="prod-thumb-link"
+															href="http://www.stadium.se/skor/damskor/sneakers/142193/converse-all-star-hi?SelectedProductUUID=n_cKd3emgfkAAAE782ky8qt7#.VPa9CbPF-dw"> <strong> <font color="#000000">699:-</font> </strong> </a> </div></td>
+                      <td style="text-align: center;"><a
+													class="prod-thumb-link"
+													href="http://www.stadium.se/varumarken/adidas/originals/214589/adidas-u-superstar?SelectedProductUUID=XarCROinfy4AAAFJ3Qd0y6ac#.VP1tXmTF-dw"> <img id="prod_thumb" title="ADIDAS" class="prod_thumb"
+														src="images/214589.jpg" alt=""> <br>
+                        <strong>ADIDAS</strong>
+                        <div style="text-align: center;"> <span class="name">U SUPERSTAR</span> </div>
+                        <span class="price"></span> </a>
+                        <div style="text-align: center;"> <a class="prod-thumb-link"
+															href="http://www.stadium.se/varumarken/adidas/originals/214589/adidas-u-superstar?SelectedProductUUID=XarCROinfy4AAAFJ3Qd0y6ac#.VP1tXmTF-dw"> <strong> <font color="#000000">899:-</font> </strong> </a> </div></td>
+                      <td style="text-align: center;"><a
+													class="prod-thumb-link"
+													href="http://www.stadium.se/skor/damskor/sneakers/209056/palladium-u-blanc-hi?SelectedProductUUID=mrjCROinPCQAAAFE1TVYoNfE#.VQL73GTF-dw"> <img id="prod_thumb" title="CONVERSE" class="prod_thumb"
+														src="images/209056.jpg" alt=""> <br>
+                        <strong>PALLADIUM</strong>
+                        <div style="text-align: center;"> <span class="name">U BLANC HI </span> </div>
+                        <span class="price"></span> </a>
+                        <div style="text-align: center;"> <a class="prod-thumb-link"
+															href="http://www.stadium.se/skor/damskor/sneakers/209056/palladium-u-blanc-hi?SelectedProductUUID=mrjCROinPCQAAAFE1TVYoNfE#.VQL73GTF-dw"> <strong> <font color="#000000">699:-</font> </strong> </a> </div></td>
+                      <td style="text-align: center;"><a
+													class="prod-thumb-link"
+													href="http://www.stadium.se/skor/damskor/sneakers/218869/converse-u-all-star-specialty-ox?SelectedProductUUID=BGfCROin9N0AAAFLaFkZPKSB#.VQL6QWTF-dw"> <img id="prod_thumb" title="CONVERSE" class="prod_thumb"
+														src="images/218869.jpg" alt=""> <br>
+                        <strong>CONVERSE</strong>
+                        <div style="text-align: center;"> <span class="name">U ALL STAR SPECIALTY OX</span> </div>
+                        <span class="price"></span> </a>
+                        <div style="text-align: center;"> <a class="prod-thumb-link"
+															href="http://www.stadium.se/skor/damskor/sneakers/218869/converse-u-all-star-specialty-ox?SelectedProductUUID=BGfCROin9N0AAAFLaFkZPKSB#.VQL6QWTF-dw"> <strong> <font color="#000000">799:-</font> </strong> </a> </div></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <br>
+              </div></td>
+          </tr>
+        </tbody>
+      </table>
+    </dl>
+  </div>
+  </div>
